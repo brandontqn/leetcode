146c4a84b931c5e2b734e4f983c7c52012594edc
Brandon Nguyen <brandon.nguyen@finning.com>
2020-07-21 23:01:32 -0700

    added solution for two_city_scheduling

--- two_city_scheduling/two_city_scheduling.py
+++ two_city_scheduling/two_city_scheduling.py
@@ -11,4 +11,33 @@
 
 class Solution:
     def twoCitySchedCost(self, costs):
-        return 110
+        n = len(costs)
+        result = -1
+
+        diff_arr = []
+        for i in range(n):
+            diff = costs[i][0] - costs[i][1]
+            diff_arr += [(i, diff)]
+
+        self.qsort(diff_arr, 0, n-1)
+        city_a = [costs[x[0]][0] for x in diff_arr[:int(n/2)]]
+        city_b = [costs[x[0]][1] for x in diff_arr[int(n/2):]]
+        result = sum(city_a) + sum(city_b)
+
+        return result
+
+    def qsort(self, arr, left, right):
+        if left >= right: return
+
+        pivot = arr[right][1]
+        pivot_position = left
+
+        for i in range(left, right+1):
+            if(arr[i][1] <= pivot):
+                tmp = arr[i]
+                arr[i] = arr[pivot_position]
+                arr[pivot_position] = tmp
+                pivot_position += 1
+
+        self.qsort(arr, left, pivot_position-2)
+        self.qsort(arr, pivot_position, right)
