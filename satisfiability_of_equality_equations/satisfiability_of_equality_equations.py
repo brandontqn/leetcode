1f9bb987c7721be42781138aebd36e1b4eb77c22
Brandon Nguyen <brandon@virtro.ca>
2020-08-15 19:39:21 -0700

    incomplete implementation for equation satisfiability problem

--- satisfiability_of_equality_equations/satisfiability_of_equality_equations.py
+++ satisfiability_of_equality_equations/satisfiability_of_equality_equations.py
@@ -9,23 +9,35 @@
 
 class Solution:
     def equationsPossible(self, equations: [str]) -> bool:
-        variables, possible = {}, {}
+        not_possible_equations = []
+        hashmap = {}
+
         arg_1_idx = 0
         arg_2_idx = 3
 
-        # preprocess, see all variables
         for equation in equations:
             arg_1 = equation[arg_1_idx]
             arg_2 = equation[arg_2_idx]
-            variables[arg_1] = True
-            variables[arg_2] = True
-            
-        # assume everything is possible
-        for variable in variables:
-            possible[variable] = [x for x in variables if x != variable]
+            if equation[1:3] == '==':
+                # if (arg_1 in hashmap.keys() and arg_2 not in hashmap[arg_1]):
+                #     hashmap[arg_1].append(arg_2)
+                # else:
+                #     hashmap[arg_1] = [arg_2]
+                # if (arg_2 in hashmap.keys() and arg_1 not in hashmap[arg_2]):
+                #     hashmap[arg_2].append(arg_1)
+                # else:
+                #     hashmap[arg_2] = [arg_1]
+            else:
+                not_possible_equations.append(equation)
         
-        return True
+        for equation in not_possible_equations:
+            arg_1 = equation[arg_1_idx]
+            arg_2 = equation[arg_2_idx]
 
-    # def comparison(equation: str) -> str:
-    #     string_comparison = equation[1:3]
-        
+            if (arg_1 in hashmap.keys() and arg_2 in hashmap[arg_1]):
+                return False
+            
+            if (arg_2 in hashmap.keys() and arg_1 in hashmap[arg_2]):
+                return False
+
+        return True
