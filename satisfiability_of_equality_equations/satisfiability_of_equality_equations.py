776e46cd291822e31317f507445cedfaa180f9bb
Brandon Nguyen <brandon.nguyen@finning.com>
2020-08-16 15:06:28 -0700

    added complete solution for equation satisfiability problem

--- satisfiability_of_equality_equations/satisfiability_of_equality_equations.py
+++ satisfiability_of_equality_equations/satisfiability_of_equality_equations.py
@@ -1,61 +1,47 @@
-# https://leetcode.com/problems/satisfiability-of-equality-equations/
+# # https://leetcode.com/problems/satisfiability-of-equality-equations/
 
-# Given an array equations of strings that represent relationships 
-# between variables, each string equations[i] has length 4 and 
-# takes one of two different forms: "a==b" or "a!=b".  
-# Here, a and b are lowercase letters (not necessarily different) 
-# that represent one-letter variable names.
-# Return true if and only if it is possible to assign integers to variable names so as to satisfy all the given equations.
+# # Given an array equations of strings that represent relationships 
+# # between variables, each string equations[i] has length 4 and 
+# # takes one of two different forms: "a==b" or "a!=b".  
+# # Here, a and b are lowercase letters (not necessarily different) 
+# # that represent one-letter variable names.
+# # Return true if and only if it is possible to assign integers to variable names so as to satisfy all the given equations.
+class Solution(object):
+    def equationsPossible(self, equations):
+        arg1_idx = 0
+        arg2_idx = 3
+        equality_idx = 1
 
-class Solution:
-    def equationsPossible(self, equations: [str]) -> bool:
-        variables = {}
+        graph = [[] for _ in range(26)]
 
-        arg_1_idx = 0
-        arg_2_idx = 3
-        for equation in equations:
-            arg_1 = equation[arg_1_idx]
-            arg_2 = equation[arg_2_idx]
-            if (arg_1 not in variables.keys()):
-                variables[arg_1] = []
-            if (arg_2 not in variables.keys()):
-                variables[arg_2] = []
-
-        not_possible_equations = []
-        for equation in equations:
-            arg_1 = equation[arg_1_idx]
-            arg_2 = equation[arg_2_idx]
-            if equation[1:3] == '==':
-                self.updateVariables(variables, arg_1, arg_2)
+        not_equations = []
+        for eqn in equations:
+            if eqn[equality_idx] == '=':
+                arg1 = ord(eqn[arg1_idx]) - ord('a')
+                arg2 = ord(eqn[arg2_idx]) - ord('a')
+                graph[arg1].append(arg2)
+                graph[arg2].append(arg1)
             else:
-                not_possible_equations.append(equation)
-        
-        for equation in not_possible_equations:
-            arg_1 = equation[arg_1_idx]
-            arg_2 = equation[arg_2_idx]
+                not_equations.append(eqn)
 
-            if arg_1 == arg_2:
-                return False
+        color = [None] * 26 # array of color assignments for each argument
+        t = 0               # color value
+        for var in range(26):
+            if color[var] is None:
+                t += 1
+                stack = [var]
+                while stack:
+                    node = stack.pop()
+                    for connected_node in graph[node]:
+                        if color[connected_node] is None:
+                            color[connected_node] = t
+                            stack.append(connected_node)
 
-            if (arg_2 in variables[arg_1]):
-                return False
-            
-            if (arg_1 in variables[arg_2]):
-                return False
-
+        for eqn in not_equations:
+            if eqn[1] == '!':
+                arg1 = ord(eqn[arg1_idx]) - ord('a')
+                arg2 = ord(eqn[arg2_idx]) - ord('a')
+                if arg1 == arg2: return False
+                if color[arg1] is not None and color[arg1] == color[arg2]:
+                    return False
         return True
-
-
-    def updateVariables(self, variables: dict, arg_1: str, arg_2: str):
-        if arg_1 == arg_2:
-            return
-
-        for i in variables[arg_1]:
-            variables[i].append(arg_2)
-            variables[arg_2].append(i)
-        variables[arg_1].append(arg_2)
-
-        for i in variables[arg_2]:
-            variables[i].append(arg_1)
-            variables[arg_1].append(i)
-        variables[arg_2].append(arg_1)
