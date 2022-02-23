eef70e3712645daa42561c5820add3194aee5ca8
Brandon Nguyen <brandontqnguyen@gmail.com>
2022-02-23 00:56:25 -0800

    working solution for optimal_aircraft_routes

--- AmazonOA/optimal_aircraft_routes/optimal_aircraft_routes.py
+++ AmazonOA/optimal_aircraft_routes/optimal_aircraft_routes.py
@@ -1,8 +1,52 @@
 """
 Given
 - a maximum distance that a plane can travel
-- a list of forward routes where forwardRoute[i] = (route_id, distance)
-- a list of return routes where returnRoute[i] = (route_id, distance)
+- a list of forward routes where forwardRoutes[i] = (route_id, distance)
+- a list of return routes where returnRoutes[i] = (route_id, distance)
 
 Return a list of (forwardRouteId, returnRouteId) tuples where the total route distance is optimal.
 """
+
+def optimalAircraftRoutes(maxDistance, forwardRoutes, returnRoutes):
+    n = len(forwardRoutes)
+
+    if n == 0:
+        return [[]]
+
+    sorted_forward_routes = sorted(forwardRoutes, key=lambda route:route[1])
+    sorted_return_routes = sorted(returnRoutes, key=lambda route:route[1])
+    optimal_distance = float("-inf")
+    optimal_distance_routes = []
+
+    for forward_id, forward_distance in sorted_forward_routes:
+        return_min_idx, return_max_idx = 0, n - 1
+        while return_min_idx <= return_max_idx:
+            return_idx = (return_min_idx + return_max_idx) // 2
+            return_id = sorted_return_routes[return_idx][0]
+            return_distance = sorted_return_routes[return_idx][1]
+            
+            curr_distance = forward_distance + return_distance
+            if curr_distance > optimal_distance and curr_distance <= maxDistance:
+                optimal_distance = curr_distance
+                optimal_distance_routes = [(forward_id, return_id)]
+                return_min_idx = return_idx + 1
+            elif curr_distance > maxDistance:
+                return_max_idx = return_idx - 1
+            elif curr_distance == optimal_distance:
+                optimal_distance_routes.append((forward_id, return_id))
+                return_min_idx = return_idx + 1
+            else: # curr_distance < optimal_distance
+                return_min_idx = return_idx + 1
+
+    return optimal_distance_routes if optimal_distance_routes else [[]]
+
+
+def main():
+    maxDistance = 3000
+    forwardRoutes = [(1, 2000), (2, 4000), (3, 5000)]
+    returnRoutes = [(1, 1000), (2, 4000), (3, 4950)]
+    result = optimalAircraftRoutes(maxDistance, forwardRoutes, returnRoutes)
+    print(result)
+
+if __name__ == "__main__":
+    main()
