8ff4333d500053faadef60d1aa51adf22073c78d
Brandon Nguyen <brandon@virtro.ca>
2022-02-23 00:01:16 -0800

    new Amazon OA Qs

---
+++ AmazonOA/optimal_aircraft_routes/optimal_aircraft_routes.py
@@ -0,0 +1,8 @@
+"""
+Given
+- a maximum distance that a plane can travel
+- a list of forward routes where forwardRoute[i] = (route_id, distance)
+- a list of return routes where returnRoute[i] = (route_id, distance)
+
+Return a list of (forwardRouteId, returnRouteId) tuples where the total route distance is optimal.
+"""
---
+++ AmazonOA/router_coverage/router_coverage.py
@@ -0,0 +1,8 @@
+"""
+Given 
+- a list of buildings where buildings[i] is the number of people in that building,
+- a list of router locations where routerLocations[i] is the index (1-indexed) location of a router corresponding to the building locations
+- a list of router ranges where routerRange[i] is the distance +/- from routerLocation[i]
+
+Return the number of buildings which are fully covered by the routers. A building is fully covered if and only if there are X routers covering X people for that building
+"""
